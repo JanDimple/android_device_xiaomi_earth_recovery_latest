b6cef267553300996f297be2b76741f5a83702d1
JanDimple <jandimple17@gmail.com>
2025-07-16 02:02:38 +0200

    earh: PBRPify

--- pb_earth.mk
+++ pb_earth.mk
@@ -9,10 +9,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 $(call inherit-product, device/xiaomi/earth/device.mk)
 
 # Inherit some common TWRP stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Product Specifics
-PRODUCT_NAME := twrp_earth
+PRODUCT_NAME := pb_earth
 PRODUCT_DEVICE := earth
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
